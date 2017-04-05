1cc17c23fccabbf21b929a19c3c7862e21385b30
Tomohiro Suwa <neoen.gsn@gmail.com>
2017-04-06 01:52:01 +0900

    Fix to add UserStore to AppStoreGroup

--- src/renderer/stores/AppStoreGroup.js
+++ src/renderer/stores/AppStoreGroup.js
@@ -2,17 +2,20 @@ import {StoreGroup} from 'almin';
 import PlaylistStore from './playlist/PlaylistStore';
 import SearchBoxStore from './search-box/SearchBoxStore';
 import FavoriteStore from './favorite/FavoriteStore';
+import UserStore from './user/UserStore';
 
 import playlistRepository from '../repositories/PlaylistRepository';
 import searchBoxRepository from '../repositories/SearchBoxRepository';
 import favoriteRepository from '../repositories/FavoriteRepository';
+import userRepository from '../repositories/UserRepository';
 
 export default class AppStoreGroup {
   static create() {
     return new StoreGroup([
       new PlaylistStore(playlistRepository),
       new SearchBoxStore(searchBoxRepository),
-      new FavoriteStore(favoriteRepository)
+      new FavoriteStore(favoriteRepository),
+      new UserStore(userRepository)
     ]);
   }
 }
