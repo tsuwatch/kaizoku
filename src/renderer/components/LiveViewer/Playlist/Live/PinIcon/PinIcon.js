4865160ef61d6224c2273458f69392c98f0c2a23
Tomohiro Suwa <neoen.gsn@gmail.com>
2017-05-04 18:49:14 +0900

    Add missing files

---
+++ src/renderer/components/LiveViewer/Playlist/Live/PinIcon/PinIcon.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import fa from 'font-awesome/css/font-awesome.css';
+import AppLocator from '../../../../../AppLocator';
+import PinLiveUseCase from '../../../../../use-cases/PinLiveUseCase.js';
+import styles from './PinIcon.css';
+
+export default class PinIcon extends React.Component {
+  static propTypes = {
+    id: React.PropTypes.string.isRequired,
+    pinned: React.PropTypes.bool.isRequired
+  };
+
+  constructor() {
+    super();
+
+    this.state = {
+      isMouseOver: false
+    };
+  }
+
+  handleMouseOver(isMouseOver) {
+    this.setState({isMouseOver});
+  }
+
+  handlePin() {
+    AppLocator.context.useCase(PinLiveUseCase.create()).execute(this.props.id);
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        {
+          this.state.isMouseOver || this.props.pinned ? (
+            <i
+              className={`${fa.fa} ${fa['fa-check-circle']} ${fa['fa-lg']} ${styles.filled}`}
+              onMouseLeave={() => ::this.handleMouseOver(false)}
+              onClick={::this.handlePin}
+            />
+          ) : (
+            <i
+              className={`${fa.fa} ${fa['fa-check-circle-o']} ${fa['fa-lg']}`}
+              onMouseEnter={() => ::this.handleMouseOver(true)}
+            />
+          )
+        }
+      </div>
+    );
+  }
+}
